5bc89f1185f0cde9fbb31d0ae409c28424ffe458
Jose Tomas <josetg01@ucm.es>
2023-09-22 12:33:49 +0200

    moslitos
    ecomido pa comer unos molsito

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BulletMovement : MonoBehaviour
+{
+    #region parameters
+    /// <summary>
+    /// Translation speed of the bullet
+    /// </summary>
+    [SerializeField]
+    private float _speedValue = 1.0f;
+    #endregion
+
+    #region references
+    private Transform _myTransform;
+    #endregion
+
+    #region properties
+    private Vector3 _speed;
+
+    public Vector3 Speed { get { return _speed; } }
+    #endregion
+
+    #region methods
+    /// <summary>
+    /// Method to set the direction for the translation of the bullet.
+    /// </summary>
+    /// <param name="newDirection">Set direction</param>
+    public void SetDirection(Vector3 newDirection)
+    {
+        //Rota al prefab hacia la dirección "newDirection".
+        _myTransform.Rotate(newDirection);
+    }
+
+    /// <summary>
+    /// Method to add speed to the bullet while keeping it module is kept constant
+    /// 1. Add the desired extra speed.
+    /// 2. Normalize speed
+    /// 3. Multiplay per _speed value
+    /// </summary>
+    /// <param name="speedToAdd">Speed to be added (vector)</param>
+    public void AddSpeed(Vector3 speedToAdd)
+    {
+        _speed = speedToAdd.normalized * _speedValue;
+    }
+
+    /// <summary>
+    /// Method to set the initial direction of the bullet
+    /// </summary>
+    /// <param name="direction">Set direction</param>
+    public void Setup(Vector2 direction)
+    {
+        //Establece la direcciñon inicial "direction".
+        SetDirection(direction);
+    }
+    #endregion
+
+    /// <summary>
+    /// Set reference to own transform
+    /// </summary>
+    void Start()
+    {
+        _myTransform = GetComponent<Transform>(); 
+        //Setup()
+    }
+
+    /// <summary>
+    /// Update position according to speed and time
+    /// </summary>
+    void Update()
+    {
+        //Problema _speed
+        //AddSpeed();
+        _myTransform.position =  new Vector3 (_myTransform.position.x + _speed.x, 0);
+        //Debug.Log(_speed);
+        
+    }
+}
---
+++ Assets/Scripts/LookAtPositionComponent.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LookAtPositionComponent : MonoBehaviour
+{
+    #region references
+    /// <summary>
+    /// Referene to own transform
+    /// </summary>
+    private Transform _myTransform;
+    #endregion
+
+    /// <summary>
+    /// Point to look at
+    /// </summary>
+    #region properties
+    private Vector3 _lookAtPosition;
+    #endregion
+
+    #region methods
+    public void SetLookAtPosition(Vector3 mousePosition)
+    {
+        //El transform mira a la posición "mousePosition".
+        _myTransform.LookAt(mousePosition);
+    }
+    #endregion
+
+    /// <summary>
+    /// Set reference to own transform
+    /// </summary>
+    void Start()
+    {
+        _myTransform = transform;
+    }
+
+    /// <summary>
+    /// Set rotation to make the player look at the lookAtPosition
+    /// This is difficult to understand, so the code has been kept for students to use it
+    /// </summary>
+    void Update()
+    {
+        SetLookAtPosition(_lookAtPosition);
+        Quaternion rotation = Quaternion.LookRotation
+        (_lookAtPosition - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+    }
+}
