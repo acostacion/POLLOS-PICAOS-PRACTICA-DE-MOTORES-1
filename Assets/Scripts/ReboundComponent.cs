01d43913d82b45b2e95f362704e0be4055f1ef58
Paulalemany <palemany@ucm.es>
2023-10-28 17:19:26 +0200

    pruebas

--- Assets/Scripts/ReboundComponent.cs
+++ Assets/Scripts/ReboundComponent.cs
@@ -20,21 +20,32 @@ public class ReboundComponent : MonoBehaviour
         {
             // Esto funciona así: el "contacts" es el punto de colisión con el obstáculo, y "[0]" es el primer punto de contacto en un array que tiene (nada mas darle).
             // El ".normal" saca el vector normal, q es la perpendicular del punto de choque del obstáculo.
-            Vector3 normal = collision.contacts[0].normal;
+            Vector3 _normal = collision.contacts[0].normal.normalized;
             //Probar los debugs cuando tenemos el obstaculo en vertical para hacer bien esto
 
             // El vector que lleva la bala.
-            Vector3 vector = _bullet.Speed;
+            Vector3 _vector = _bullet.Speed;
 
 
             // Según la API de Unity, "Reflect" hace esto: "Refleja un vector fuera del plano definido por una normalidad.", entonces lo que hay que darle es 
             // "vector": el vector que se va a reflejar y "normal": la normal de la superficie con respecto a la cual se va a realizar la reflexión.
             // El vector se normaliza para obtener unicamente su dirección.
-            Vector3 _direction = Vector3.Reflect(vector, normal);
+            //Vector3 _direction = Vector3.Reflect(vector, normal);
+            Vector3 _direction = _vector - 2 * (Vector3.Dot(_normal, _vector) * _normal);
 
             // Establecemos la nueva dirección "direction".
             //Set direction editado
+            //if(_normal == new Vector3(0,1,0))
+            //{
+            //    Debug.Log("pa abajo");
+            //    _direction.y = -_direction.y;
+            //}
             _bullet.SetDirection(_direction);
+
+            Debug.DrawRay(_bullet.transform.position, _vector, Color.blue,36000);
+            Debug.DrawRay(this.transform.position, _normal, Color.red, 36000);
+            Debug.DrawRay(_bullet.transform.position, _direction, Color.green, 36000);
+            Debug.Log(_normal);
         }
     }
     #endregion
