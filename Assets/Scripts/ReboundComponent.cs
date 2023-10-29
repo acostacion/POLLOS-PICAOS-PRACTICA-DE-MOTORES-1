1502370602f8924eee44901a792d40d0de7bc0f6
Bienen27 <josetg01@ucm.es>
2023-10-29 12:25:41 +0100

    formula profe

--- Assets/Scripts/ReboundComponent.cs
+++ Assets/Scripts/ReboundComponent.cs
@@ -18,20 +18,41 @@ public class ReboundComponent : MonoBehaviour
 
         if(_bullet != null)
         {
+            // ANTES DE NADA MIRAR EL CÓDIGO CON LA CHULETA DEL PROFE AL LADO.
+
+            // Datos: d (_vector), n (_normal), n' (-_normal), t, z, pt, pn.
+
             // Esto funciona así: el "contacts" es el punto de colisión con el obstáculo, y "[0]" es el primer punto de contacto en un array que tiene (nada mas darle).
             // El ".normal" saca el vector normal, q es la perpendicular del punto de choque del obstáculo.
-            Vector3 _normal = collision.contacts[0].normal.normalized;
+            Vector3 _normal = collision.contacts[0].normal.normalized; // n
             //Probar los debugs cuando tenemos el obstaculo en vertical para hacer bien esto
 
             // El vector que lleva la bala.
-            Vector3 _vector = _bullet.Speed;
+            Vector3 _vector = _bullet.Speed; // d
+
+            // La dirección es w = ((n' * d) * n') + ((t * d) * t)
+            //                           pn                pt
+
+            Vector3 t = Vector3.Cross(Vector3.forward, -_normal); // t = z (Vector3.forward) x n'
+
+            Vector3 _direction = Vector3.Cross(Vector3.Cross(_vector, -_normal), -_normal) + Vector3.Cross(Vector3.Cross(t, _vector), t);
+            //                                                    ((d * n')    *    n')    +                               ((t * d) * t)
+
+
+
+
+
+
+
+
+
 
 
             // Según la API de Unity, "Reflect" hace esto: "Refleja un vector fuera del plano definido por una normalidad.", entonces lo que hay que darle es 
             // "vector": el vector que se va a reflejar y "normal": la normal de la superficie con respecto a la cual se va a realizar la reflexión.
             // El vector se normaliza para obtener unicamente su dirección.
             //Vector3 _direction = Vector3.Reflect(vector, normal);
-            Vector3 _direction = _vector - 2 * (Vector3.Dot(_normal, _vector) * _normal);
+            //Vector3 _direction = _vector - 2 * (Vector3.Dot(_vector, _normal) * _normal);
 
             // Establecemos la nueva dirección "direction".
             //Set direction editado
@@ -40,12 +61,14 @@ public class ReboundComponent : MonoBehaviour
             //    Debug.Log("pa abajo");
             //    _direction.y = -_direction.y;
             //}
-            _bullet.SetDirection(_direction);
 
-            Debug.DrawRay(_bullet.transform.position, _vector, Color.blue,36000);
-            Debug.DrawRay(this.transform.position, _normal, Color.red, 36000);
-            Debug.DrawRay(_bullet.transform.position, _direction, Color.green, 36000);
-            Debug.Log(_normal);
+            _bullet.SetDirection(_direction); // w.
+
+            //// Lo del debug.
+            //Debug.DrawRay(_bullet.transform.position, _vector, Color.blue,36000);
+            //Debug.DrawRay(this.transform.position, _normal, Color.red, 36000);
+            //Debug.DrawRay(_bullet.transform.position, _direction, Color.green, 36000);
+            //Debug.Log(_normal);
         }
     }
     #endregion
